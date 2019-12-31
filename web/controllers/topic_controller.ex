cd7f5dee07477824b5d3697c50aaae2487dac8d7
Mitchell Williams <mscottwilliams1994@gmail.com>
2019-12-30 23:07:14 -0600

    update

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -19,7 +19,7 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "topic Created")
         |> redirect(to: topic_path(conn, :index))
@@ -28,6 +28,29 @@ defmodule Discuss.TopicController do
         conn
         |> put_flash(:error, "Couldn't submit Topic")
         |> render("new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+    # changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset)
     end
   end
 end
